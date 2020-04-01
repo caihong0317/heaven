688624bac983856a3cdcbfeebdd434e2fd832fd4
caihong <caihong@163.com>
2020-04-01 22:59:33 +0800

    第二次修改

--- git_test/src/main/java/com/caihong/learn/HelloGit.java
+++ git_test/src/main/java/com/caihong/learn/HelloGit.java
@@ -1,11 +1,40 @@
 package com.caihong.learn;
 
+import java.io.File;
+
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
     }
+
+    /**
+     * 删除文件
+     * @param srcFile
+     */
+    public static void deleteFile(File srcFile){
+        if (srcFile.isFile()) {
+            srcFile.delete();
+        } else {
+            File[] listFiles = srcFile.listFiles();
+            //不是空目录
+            if (listFiles.length>0) {
+                for (File file : listFiles) {
+                    if (file.isFile()) {
+                        file.delete();
+                    } else {
+                        deleteFile(file);
+                    }
+                    file.delete();
+                }
+            }else {
+                //删除空目录
+                srcFile.delete();
+            }
+        }
+    }
+
     public  void add(int a, int b){
         System.out.println(a +b);
     }
